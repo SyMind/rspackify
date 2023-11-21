bf642709edd5b505413eb1bf75270b5d04f8085e
SyMind <dacongsama@live.com>
2023-11-21 21:08:27 +0800

    chore: adjust configuration of context for diff

--- client/viewer.js
+++ client/viewer.js
@@ -2,7 +2,9 @@ import { createTwoFilesPatch } from 'diff';
 import { html } from 'diff2html';
 import 'diff2html/bundles/css/diff2html.min.css';
 
-const diffOutput = createTwoFilesPatch('webpack', 'rspack', window.webpackOptions, window.rspackOptions);
+const diffOutput = createTwoFilesPatch('webpack', 'rspack', window.webpackOptions, window.rspackOptions, undefined, undefined, {
+  context: Number.MAX_SAFE_INTEGER
+});
 
 const diffHtml = html(diffOutput, {
   drawFileList: true,
