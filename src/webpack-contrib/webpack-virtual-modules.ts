618b06c433fb9d68904b97e560f6166629c9bc72
Cong-Cong <dacongsama@live.com>
2024-07-18 13:39:18 +0800

    fix: VirtualModuleWebpackPlugin can pass nothing

--- src/webpack-contrib/webpack-virtual-modules.ts
+++ src/webpack-contrib/webpack-virtual-modules.ts
@@ -1,8 +1,8 @@
 import { RspackVirtualModulePlugin } from 'rspack-plugin-virtual-module';
 
 class RspackifyVirtualModulePlugin extends RspackVirtualModulePlugin {
-    constructor(staticModules: Record<string, string>, tempDir?: string) {
-        super(staticModules, tempDir);
+    constructor(modules?: Record<string, string>) {
+        super(modules ?? {});
         // fix: follow error in umijs
         // TypeError: Cannot read private member from an object whose class did not declare it
         this.apply = this.apply.bind(this);
